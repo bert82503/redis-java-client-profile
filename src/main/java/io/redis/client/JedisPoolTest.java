28f1dd87a6866281ad9a889fc92220d1f7b004be
huagang.li <huagang.li@fraudmetrix.cn>
2014-12-09 21:08:38 +0800

    [Fix] 通过jedis.close()将连接返回到到连接池

--- src/main/java/io/redis/client/JedisPoolTest.java
+++ src/main/java/io/redis/client/JedisPoolTest.java
@@ -22,105 +22,103 @@ import redis.clients.jedis.JedisPoolConfig;
  */
 public class JedisPoolTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(JedisPoolTest.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(JedisPoolTest.class);
 
-    private JedisPool           pool;
+	private JedisPool pool;
 
-    @BeforeClass
-    public void init() {
-        GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(10);
-        poolConfig.setMaxIdle(10);
-        poolConfig.setMinIdle(1);
-        poolConfig.setBlockWhenExhausted(false);
-        poolConfig.setMaxWaitMillis(TimeUnit.MILLISECONDS.toMillis(30L));
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
+	@BeforeClass
+	public void init() {
+		GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
+		poolConfig.setMaxTotal(10);
+		poolConfig.setMaxIdle(10);
+		poolConfig.setMinIdle(1);
+		poolConfig.setBlockWhenExhausted(false);
+		poolConfig.setMaxWaitMillis(TimeUnit.MILLISECONDS.toMillis(30L));
+		// poolConfig.setTestOnBorrow(true);
 
-        String host = "127.0.0.1";
-        int port = 10001;
-        int timeout = (int) TimeUnit.MILLISECONDS.toMillis(100L);
+		String host = "127.0.0.1";
+		int port = 10001;
+		int timeout = (int) TimeUnit.MILLISECONDS.toMillis(100L);
 
-        this.pool = new JedisPool(poolConfig, host, port, timeout);
-    }
+		this.pool = new JedisPool(poolConfig, host, port, timeout);
+	}
 
-    /**
-     * 测试 Strings 操作。
-     */
-    @Test
-    public void doString() {
-        Jedis jedis = this.pool.getResource();
+	/**
+	 * 测试 Strings 操作。
+	 */
+	@Test
+	public void doString() {
+		String key = "hello";
+		String value = "world";
 
-        String key = "hello";
-        String value = "world";
+		Jedis jedis = this.pool.getResource();
 
-        /*
-         * 一组逻辑操作
-         */
-        // get
-        String v = jedis.get(key);
-        assertEquals(v, null);
-        // set
-        String statusCode = jedis.set(key, value);
-        assertEquals(statusCode, "OK");
-        // get
-        v = jedis.get(key);
-        assertEquals(v, "world");
-        // delete
-        Long delNum = jedis.del(key);
-        assertEquals(delNum.longValue(), 1L);
+		// 一组逻辑操作
+		// get
+		String v = jedis.get(key);
+		assertEquals(v, null);
+		// set
+		String statusCode = jedis.set(key, value);
+		assertEquals(statusCode, "OK");
+		// get
+		v = jedis.get(key);
+		assertEquals(v, "world");
+		// delete
+		Long delNum = jedis.del(key);
+		assertEquals(delNum.longValue(), 1L);
 
-        this.pool.returnResource(jedis);
-    }
+		jedis.close();
+	}
 
-    /**
-     * 验证连接池功能。
-     * <p>
-     * 
-     * <pre>
-     * 通过"sudo netstat -anp | grep nut"定期查看程序运行时，仅有如下一条类似信息(10001是nutcracker的端口号)：
-     *      tcp        0      0 127.0.0.1:10001         127.0.0.1:43446         ESTABLISHED 17636/nutcracker
-     * </pre>
-     * 
-     * <font color="red">测试发现，一次命令操作都会触发nutcracker与后端redis服务器重新建立一条新的链接！</font>
-     * 
-     * @throws InterruptedException
-     */
-    @Test
-    public void verifyConnectionPoolFeature() throws InterruptedException {
-        Jedis jedis = this.pool.getResource();
+	/**
+	 * 验证连接池功能。
+	 * <p>
+	 * 
+	 * <pre>
+	 * 通过"sudo netstat -anp | grep nut"定期查看程序运行时，仅有如下一条类似信息(10001是nutcracker的端口号)：
+	 *      tcp        0      0 127.0.0.1:10001         127.0.0.1:43446         ESTABLISHED 17636/nutcracker
+	 * </pre>
+	 * 
+	 * <font color="red">测试发现，一次命令操作都会触发nutcracker与后端redis服务器重新建立一条新的链接！</font>
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void verifyConnectionPoolFeature() throws InterruptedException {
+		Jedis jedis = this.pool.getResource();
 
-        String key = "pool";
-        String value = "yes";
+		String key = "pool";
+		String value = "yes";
 
-        for (int i = 0; i < 3; i++) {
-            // set
-            String statusCode = jedis.set(key, value);
-            assertEquals(statusCode, "OK");
+		for (int i = 0; i < 3; i++) {
+			// set
+			String statusCode = jedis.set(key, value);
+			assertEquals(statusCode, "OK");
 
-            // get
-            String v = jedis.get(key);
-            assertEquals(v, "yes");
+			// get
+			String v = jedis.get(key);
+			assertEquals(v, "yes");
 
-            // delete
-            Long delNum = jedis.del(key);
-            assertEquals(delNum.longValue(), 1L);
+			// delete
+			Long delNum = jedis.del(key);
+			assertEquals(delNum.longValue(), 1L);
 
-            if (logger.isInfoEnabled()) {
-                logger.info("Complete time: {}", Integer.valueOf(i));
-            }
+			if (logger.isInfoEnabled()) {
+				logger.info("Complete time: {}", Integer.valueOf(i));
+			}
 
-            TimeUnit.SECONDS.sleep(10L);
-        }
+			TimeUnit.SECONDS.sleep(10L);
+		}
 
-        this.pool.returnResource(jedis);
-    }
+		jedis.close();
+	}
 
-    @AfterClass
-    public void destroy() {
-        if (this.pool != null) {
-            this.pool.close();
-        }
-    }
+	@AfterClass
+	public void destroy() {
+		if (this.pool != null) {
+			this.pool.close();
+		}
+	}
 
 }
--- src/main/java/io/redis/client/ShardedJedisPoolTest.java
+++ src/main/java/io/redis/client/ShardedJedisPoolTest.java
@@ -26,6 +26,7 @@ import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisShardInfo;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
+import redis.clients.jedis.exceptions.JedisException;
 
 /**
  * 类ShardedJedisPoolTest.java的实现描述：{@link ShardedJedisPool} tutorial
@@ -34,230 +35,277 @@ import redis.clients.jedis.ShardedJedisPool;
  */
 public class ShardedJedisPoolTest {
 
-    private static final Logger logger          = LoggerFactory.getLogger(ShardedJedisPoolTest.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(ShardedJedisPoolTest.class);
 
-    private static final String DEFAULT_HOST    = "127.0.0.1";
+	private static final String DEFAULT_HOST = "127.0.0.1";
 
-    private static final int    DEFAULT_TIMEOUT = (int) TimeUnit.SECONDS.toMillis(1L);
+	private static final int DEFAULT_TIMEOUT = (int) TimeUnit.SECONDS
+			.toMillis(1L);
 
-    private ShardedJedisPool    pool;
+	private ShardedJedisPool pool;
 
-    @BeforeClass
-    public void init() {
-        // 获取 twemproxy 服务器列表
-        List<HostAndPort> hosts = this.getHostList(ServerFlag.TWEMPROXY);
+	@BeforeClass
+	public void init() {
+		// 获取 twemproxy 服务器列表
+		List<HostAndPort> hosts = this.getHostList(ServerFlag.REDIS);
 
-        List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>(hosts.size());
-        for (HostAndPort hostInfo : hosts) {
-            JedisShardInfo shardInfo = new JedisShardInfo(hostInfo.getHost(), hostInfo.getPort(), DEFAULT_TIMEOUT);
-            shards.add(shardInfo);
-        }
+		List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>(
+				hosts.size());
+		for (HostAndPort hostInfo : hosts) {
+			JedisShardInfo shardInfo = new JedisShardInfo(hostInfo.getHost(),
+					hostInfo.getPort(), DEFAULT_TIMEOUT);
+			shards.add(shardInfo);
+		}
 
-        GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
-        // 高并发压测
-        poolConfig.setMaxTotal(65536);
-        poolConfig.setMaxIdle(65536);
-        poolConfig.setMinIdle(3);
-        poolConfig.setBlockWhenExhausted(false);
-        poolConfig.setMaxWaitMillis(TimeUnit.MILLISECONDS.toMillis(30L));
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
+		GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
+		// 高并发压测
+		poolConfig.setMaxTotal(32768);
+		poolConfig.setMaxIdle(32768);
+		poolConfig.setMinIdle(3);
 
-        this.pool = new ShardedJedisPool(poolConfig, shards);
-    }
+		// 非阻塞
+		poolConfig.setBlockWhenExhausted(false);
+		// 阻塞等待一段时间
+		// poolConfig.setBlockWhenExhausted(true);
+		// poolConfig.setMaxWaitMillis(TimeUnit.MILLISECONDS.toMillis(10L));
 
-    private enum ServerFlag {
-        REDIS, TWEMPROXY
-    }
+		// 在借用池对象时，检测其有效性
+		// poolConfig.setTestOnBorrow(true);
 
-    private List<HostAndPort> getHostList(ServerFlag serverFlag) {
-        int[] ports = null;
-        switch (serverFlag) {
-            case REDIS:
-                // Redis servers
-                ports = new int[] { 6379, 6380, 6381 };
-                break;
-            case TWEMPROXY:
-                // Twemproxy servers
-                ports = new int[] { 10001 };
-                break;
-            default:
-                throw new IllegalArgumentException("Not has this server flag: " + serverFlag);
-        }
+		this.pool = new ShardedJedisPool(poolConfig, shards);
+	}
 
-        List<HostAndPort> hosts = new ArrayList<HostAndPort>(ports.length);
-        for (int port : ports) {
-            hosts.add(new HostAndPort(DEFAULT_HOST, port));
-        }
-        return hosts;
-    }
+	private enum ServerFlag {
+		REDIS, TWEMPROXY
+	}
 
-    private static final String TWEMPROXY_FILE = "twemproxy.txt";
+	private List<HostAndPort> getHostList(ServerFlag serverFlag) {
+		int[] ports = null;
+		switch (serverFlag) {
+			case REDIS :
+				// Redis servers
+				ports = new int[]{6379, 6380, 6381};
+				break;
+			case TWEMPROXY :
+				// Twemproxy servers
+				ports = new int[]{10001};
+				break;
+			default :
+				throw new IllegalArgumentException("Not has this server flag: "
+						+ serverFlag);
+		}
 
-    private static final String DEFAUL_VALUE   = "1";
+		List<HostAndPort> hosts = new ArrayList<HostAndPort>(ports.length);
+		for (int port : ports) {
+			hosts.add(new HostAndPort(DEFAULT_HOST, port));
+		}
+		return hosts;
+	}
 
-    private static final String RET_OK         = "OK";
+	private static final String TWEMPROXY_FILE = "twemproxy.txt";
 
-    private static final int    SIZE           = 10;
+	private static final String DEFAUL_VALUE = "1";
 
-    private AtomicInteger       counter        = new AtomicInteger(1);
+	private static final String RET_OK = "OK";
 
-    /**
-     * 高并发压力测试。
-     * <p>
-     * 因为Twemproxy与后端redis服务器在每执行一条命令时，都会创建一条新的链接。
-     */
-    @Test(invocationCount = 20000, threadPoolSize = 1024)
-    public void highConcurrencyStressTest() {
-        long start = System.currentTimeMillis();
+	private static final int SIZE = 7;
 
-        // 获取一条Redis连接
-        ShardedJedis jedis = this.pool.getResource();
-        // SET
-        String key = "st_" + counter.getAndIncrement();
-        String statusCode = jedis.set(key, DEFAUL_VALUE);
-        assertEquals(statusCode, RET_OK);
-        // 返回连接到连接池
-        this.pool.returnResource(jedis);
+	private AtomicInteger counter = new AtomicInteger(1);
 
-        long runTime = System.currentTimeMillis() - start;
-        if (runTime > 10) {
-            logger.info("{}'s run time: {}", key, runTime);
-        }
-    }
+	@Test
+	public void serverDown() throws InterruptedException {
+		String key = null;
 
-    /**
-     * 测试 Twemproxy 的 "Shard data automatically across multiple servers (跨多台服务器自动切分数据)" 功能。
-     * <p>
-     * <font color="red">注意：记得调整随机样本数量({@link #SIZE})的值！建议该值被设置为大于1000</font>
-     * 
-     * @throws IOException
-     */
-    @Test
-    public void sharding() {
-        ShardedJedis jedis = this.pool.getResource();
+		for (int i = 1; i <= SIZE; i++) {
+			ShardedJedis jedis = null;
 
-        PrintWriter writer = null;
+			key = "st_" + i;
+			try {
+				// 获取一条Redis连接
+				jedis = this.pool.getResource();
+				JedisShardInfo shardInfo = jedis.getShardInfo(key);
+				logger.info("Shard Info: " + shardInfo);
 
-        try {
-            writer = new PrintWriter(new FileWriter(TWEMPROXY_FILE));
+				try {
+					String statusCode = jedis.set(key, DEFAUL_VALUE);
+					assertEquals(statusCode, RET_OK);
+					jedis.close();
+				} catch (JedisException je) {
+					logger.warn("Failed to operate on Jedis Client", je);
+				}
 
-            // 样本数量
-            Random rand = new Random();
-            for (int i = 0; i < SIZE; i++) {
-                int v = rand.nextInt();
-                String key = Integer.toString(v);
-                writer.println(key);
-                // SET
-                jedis.set(key, DEFAUL_VALUE);
-            }
-        } catch (IOException e) {
-            System.err.println(e);
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-    }
+				logger.info("Complete time: {}", Integer.valueOf(i));
+				if (i < SIZE) {
+					TimeUnit.SECONDS.sleep(5L);
+				}
 
-    /**
-     * 验证 Twemproxy 的"跨多台服务器自动切分数据"功能。
-     * <p>
-     * 通过统计随机样本 Keys 的"命中率"，分布在每台服务器的Key的总数差不多，分布还算均匀。
-     * 
-     * @throws IOException
-     */
-    @Test
-    public void checkSharding() throws IOException {
-        // 读取待测试 Key
-        File file = new File(TWEMPROXY_FILE);
-        List<String> keys = FileUtils.readLines(file);
+			} catch (Exception e) {
+				// failed to borrow or return an object
+				logger.warn("Failed to borrow or return a pooled object", e);
+			}
+		}
+	}
 
-        /*
-         * 1. 获取 redis 服务器列表； 2. 对每台 redis 服务器循环进行 Key GET 操作，获取命中数。
-         */
-        List<HostAndPort> hostList = this.getHostList(ServerFlag.REDIS);
-        for (HostAndPort hostInfo : hostList) {
-            List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>(1);
-            JedisShardInfo shardInfo = new JedisShardInfo(hostInfo.getHost(), hostInfo.getPort(), DEFAULT_TIMEOUT);
-            shards.add(shardInfo);
+	/**
+	 * 高并发压力测试。
+	 * <p>
+	 * 因为Twemproxy与后端redis服务器在每执行一条命令时，都会创建一条新的链接。
+	 */
+	@Test(invocationCount = 20000, threadPoolSize = 1024)
+	public void highConcurrencyStressTest() {
+		long start = System.currentTimeMillis();
 
-            GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
-            poolConfig.setMaxTotal(10);
-            poolConfig.setMaxIdle(10);
-            poolConfig.setMinIdle(3);
-            poolConfig.setMaxWaitMillis(TimeUnit.SECONDS.toMillis(1L));
-            poolConfig.setTestOnBorrow(true);
+		// 获取一条Redis连接
+		ShardedJedis jedis = this.pool.getResource();
+		// SET
+		String key = "st_" + counter.getAndIncrement();
+		String statusCode = jedis.set(key, DEFAUL_VALUE);
+		assertEquals(statusCode, RET_OK);
+		// 返回连接到连接池
+		jedis.close();
 
-            ShardedJedisPool jedisPool = new ShardedJedisPool(poolConfig, shards);
-            try {
-                ShardedJedis jedis = jedisPool.getResource();
+		long runTime = System.currentTimeMillis() - start;
+		if (runTime > 10) {
+			logger.info("{}'s run time: {}", key, runTime);
+		}
+	}
 
-                // 统计命中率
-                int hits = 0;
-                for (String key : keys) {
-                    String value = jedis.get(key);
-                    if (!StringUtils.isEmpty(value)) {
-                        ++hits;
-                        // 清理缓存数据
-                        jedis.del(key);
-                    }
-                }
+	/**
+	 * 测试 Twemproxy 的
+	 * "Shard data automatically across multiple servers (跨多台服务器自动切分数据)" 功能。
+	 * <p>
+	 * <font color="red">注意：记得调整随机样本数量({@link #SIZE})的值！建议该值被设置为大于1000</font>
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void sharding() {
+		PrintWriter writer = null;
 
-                if (logger.isInfoEnabled()) {
-                    logger.info("Host {}'s hit ratio: {}", hostInfo, hits);
-                }
-            } finally {
-                jedisPool.close();
-            }
+		try {
+			writer = new PrintWriter(new FileWriter(TWEMPROXY_FILE));
 
-        }
-    }
+			ShardedJedis jedis = this.pool.getResource();
+			// 样本数量
+			Random rand = new Random();
+			for (int i = 0; i < SIZE; i++) {
+				int v = rand.nextInt();
+				String key = Integer.toString(v);
+				writer.println(key);
+				// SET
+				jedis.set(key, DEFAUL_VALUE);
+			}
+			jedis.close();
+		} catch (IOException e) {
+			System.err.println(e);
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
 
-    /**
-     * 验证 Twemproxy 的 "auto-failover" 功能。
-     * <p>
-     * 当某个节点宕掉时，Twemproxy可以自动将它从集群中剔除； 而当它恢复服务时，Twemproxy也会自动连接。
-     * <p>
-     * 随机样本 Keys 通过 {@link #sharding()} 方法自动生成，并写到 {@link #TWEMPROXY_FILE} 文件中。<br>
-     * <font color="red">但要注意：记得调整随机样本数量({@link #SIZE})的值！建议该值被设置为小于20</font>
-     */
-    @Test
-    public void autoFailover() {
-        ShardedJedis jedis = null;
-        String[] keys = { "-640595852", // 6379
-                "563752978", // 6379
-                "1297026184", // 6380
-                "843626243", // 6380, 6379
-                "579761811", // 6379
-                "-1148481590", // 6379
-                "-1653107798", // 6381
-                "1208161125", // 6380, 6381
-                "549094194", // 6381
-                "1082579570", // 6380, 6381
-        };
-        String statusCode = null;
+	/**
+	 * 验证 Twemproxy 的"跨多台服务器自动切分数据"功能。
+	 * <p>
+	 * 通过统计随机样本 Keys 的"命中率"，分布在每台服务器的Key的总数差不多，分布还算均匀。
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void checkSharding() throws IOException {
+		// 读取待测试 Key
+		File file = new File(TWEMPROXY_FILE);
+		List<String> keys = FileUtils.readLines(file);
 
-        for (String key : keys) {
-            try {
-                // 获取一条Redis连接
-                jedis = this.pool.getResource();
-                // SET
-                statusCode = jedis.set(key, DEFAUL_VALUE);
-                assertEquals(statusCode, RET_OK);
-                // 返回连接到连接池
-                this.pool.returnResourceObject(jedis);
-            } catch (Exception e) {
-                logger.error("Set key error: {}", key);
-            }
-        }
-    }
+		/*
+		 * 1. 获取 redis 服务器列表； 2. 对每台 redis 服务器循环进行 Key GET 操作，获取命中数。
+		 */
+		List<HostAndPort> hostList = this.getHostList(ServerFlag.REDIS);
+		for (HostAndPort hostInfo : hostList) {
+			List<JedisShardInfo> shards = new ArrayList<JedisShardInfo>(1);
+			JedisShardInfo shardInfo = new JedisShardInfo(hostInfo.getHost(),
+					hostInfo.getPort(), DEFAULT_TIMEOUT);
+			shards.add(shardInfo);
 
-    @AfterClass
-    public void destroy() {
-        if (this.pool != null) {
-            this.pool.close();
-        }
-    }
+			GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
+			poolConfig.setMaxTotal(10);
+			poolConfig.setMaxIdle(10);
+			poolConfig.setMinIdle(3);
+			poolConfig.setMaxWaitMillis(TimeUnit.SECONDS.toMillis(1L));
+			poolConfig.setTestOnBorrow(true);
+
+			ShardedJedisPool jedisPool = new ShardedJedisPool(poolConfig,
+					shards);
+			try {
+				ShardedJedis jedis = jedisPool.getResource();
+
+				// 统计命中率
+				int hits = 0;
+				for (String key : keys) {
+					String value = jedis.get(key);
+					if (!StringUtils.isEmpty(value)) {
+						++hits;
+						// 清理缓存数据
+						jedis.del(key);
+					}
+				}
+
+				if (logger.isInfoEnabled()) {
+					logger.info("Host {}'s hit ratio: {}", hostInfo, hits);
+				}
+			} finally {
+				jedisPool.close();
+			}
+
+		}
+	}
+
+	/**
+	 * 验证 Twemproxy 的 "auto-failover" 功能。
+	 * <p>
+	 * 当某个节点宕掉时，Twemproxy可以自动将它从集群中剔除； 而当它恢复服务时，Twemproxy也会自动连接。
+	 * <p>
+	 * 随机样本 Keys 通过 {@link #sharding()} 方法自动生成，并写到 {@link #TWEMPROXY_FILE} 文件中。<br>
+	 * <font color="red">但要注意：记得调整随机样本数量({@link #SIZE})的值！建议该值被设置为小于20</font>
+	 */
+	@Test
+	public void autoFailover() {
+		ShardedJedis jedis = null;
+		String[] keys = {"-640595852", // 6379
+				"563752978", // 6379
+				"1297026184", // 6380
+				"843626243", // 6380, 6379
+				"579761811", // 6379
+				"-1148481590", // 6379
+				"-1653107798", // 6381
+				"1208161125", // 6380, 6381
+				"549094194", // 6381
+				"1082579570", // 6380, 6381
+		};
+		String statusCode = null;
+
+		for (String key : keys) {
+			try {
+				// 获取一条Redis连接
+				jedis = this.pool.getResource();
+				// SET
+				statusCode = jedis.set(key, DEFAUL_VALUE);
+				assertEquals(statusCode, RET_OK);
+				// 返回连接到连接池
+				jedis.close();
+			} catch (Exception e) {
+				logger.error("Set key error: {}", key);
+			}
+		}
+	}
+
+	@AfterClass
+	public void destroy() {
+		if (this.pool != null) {
+			this.pool.close();
+		}
+	}
 
 }
